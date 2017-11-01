484c52d1679b6ec1b14e23b95226677fbc72aeae
kenpeter <kenpeter>
2017-11-01 15:12:03 +1100

    update

---
+++ data/country.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'China'},
+  {id: 2, name: 'Australia'},
+  {id: 3, name: 'USA'},
+  {id: 4, name: 'Spain'}
+]
---
+++ data/institution.js
@@ -0,0 +1,10 @@
+export default [
+  {id: 1, countryId: 1, name: 'China 1'},
+  {id: 2, countryId: 1, name: 'China 2'},
+  {id: 3, countryId: 2, name: 'Australia 1'},
+  {id: 4, countryId: 2, name: 'Australia 2'},
+  {id: 5, countryId: 3, name: 'USA 1'},
+  {id: 6, countryId: 3, name: 'USA 2'},
+  {id: 7, countryId: 4, name: 'Spain 1'},
+  {id: 8, countryId: 4, name: 'Spain 2'},
+]
