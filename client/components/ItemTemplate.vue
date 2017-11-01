6e9ce9919b874c16c23194ffc5bc1427b35b8eca
kenpeter <kenpeter>
2017-11-01 11:27:41 +1100

    kind of working

---
+++ client/components/ItemTemplate.vue
@@ -0,0 +1,17 @@
+<!-- parse the item -->
+<template>
+  <div>
+    <b>#{{ item.id }}</b>
+    <span>{{ item.name }}</span>
+    <abbr>{{ item.description }}</abbr>
+  </div>
+</template>
+
+<script>
+export default {
+  // Like prop type in react
+  props: { 
+    item: { required: true } 
+  }
+}
+</script>
--- client/components/MyAutoComplete.vue
+++ client/components/MyAutoComplete.vue
@@ -1,13 +1,53 @@
+<!-- we don't need to use this.items -->
+<!-- item, input, v model -->
+<!-- label -->
+<!-- sub component -->
+<!-- Filter item -->
 <template>
-  <h1>sub comp</h1>
+  <v-autocomplete 
+    :items="items"
+    v-model="item"
+    :get-label="getLabel"
+    :component-item='template'
+    
+    @update-items="updateItems"
+  >
+  </v-autocomplete>
 </template>
 
 <script>
+import ItemTemplate from './ItemTemplate.vue'
 
 export default {
   data () {
     return {
-      
+      // Single item
+      item: {id: 9, name: 'Lion', description: 'Lion text'},
+      // Many items
+      items: [],
+      // 
+      template: ItemTemplate
+    }
+  },
+
+  methods: {
+    // Label
+    getLabel (item) {
+      return item.name
+    },
+
+    // Filter items
+    updateItems (text) {
+      /*
+      yourGetItemsMethod(text).then( (response) => {
+        this.items = response
+      })
+      */
+      this.items = [
+        {id: 9, name: 'Lion', description: 'Lion text'},
+        {id: 10, name: 'Cat', description: 'Cat text'}
+      ]
+
     }
   }
 }
